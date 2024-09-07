721d71f2edaed8f87edac7b73df1cf1dc0be17f0
Jaekwan <wprhks536@gmail.com>
2024-09-07 17:44:09 +0900

    feat: cadifyWAS base

---
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/CadifyWasApplication.java
@@ -0,0 +1,13 @@
+package com.cadify.cadifyWAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadifyWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadifyWasApplication.class, args);
+	}
+
+}
