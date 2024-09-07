28d5af4104802c8a49b8f36ab7229d3524e5e17c
Jaekwan <wprhks536@gmail.com>
2024-09-07 17:44:09 +0900

    feat: cadifyWAS base

---
+++ cadifyWAS/src/test/java/com/cadify/cadifyWAS/CadifyWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadify.cadifyWAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadifyWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
