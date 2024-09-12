036699b815244035009d5948788bedb1e80bd8c2
Jaekwan <wprhks536@gmail.com>
2024-09-12 21:30:12 +0900

    feat: jwt token

---
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.cadify.cadifyWAS.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        if(Objects.isNull(authorization));
+    }
+}
--- cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/jwt/JwtUtil.java
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/jwt/JwtUtil.java
@@ -14,7 +14,7 @@ public class JwtUtil {
     private String secretKey;
     @Value("${jwt.expiration")
     private long expirationTime;
-
+    // 토큰생성
     public String generateToken(String username){
         return Jwts.builder()
                 .setSubject(username)
@@ -23,11 +23,21 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
-
+    // claim 추출(payload 부분)
     public Claims extractClaims(String token){
         return Jwts.parser()
                 .setSigningKey(secretKey)
-                .parseClaimsJws(token)
+                .parseClaimsJws(token)// 서명 검증
                 .getBody();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token).getSubject();
+    }
+    public boolean isTokenExpired(String token){
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+    public boolean validateToken(String token, String username){
+        return(username.equals(extractUsername(token))) && !isTokenExpired(token);
     }
 }
