45c7b08d05d4ee8d37e3dd72e95a2965da1dcb93
Jaekwan <wprhks536@gmail.com>
2024-10-03 19:47:58 +0900

    feat: global exception handle

--- cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/SecurityConfig.java
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
--- cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/jwt/JwtProvider.java
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/security/jwt/JwtProvider.java
@@ -1,12 +1,13 @@
 package com.cadify.cadifyWAS.security.jwt;
 
-import com.cadify.cadifyWAS.exception.CustomLogicException;
-import com.cadify.cadifyWAS.exception.ExceptionCode;
 import io.jsonwebtoken.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
@@ -15,13 +16,18 @@ public class JwtProvider {
     private String secretKey;
     @Value("${jwt.expiration}")
     private long expirationTime;
+
     // 토큰생성
     public String generateToken(String username){
+        byte[] secretBytes = Base64.getDecoder().decode(secretKey);
+        SecretKey byteKey = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS256.getJcaName());
+
+
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(byteKey, SignatureAlgorithm.HS256)
                 .compact();
     }
     // claim 추출(payload 부분)
@@ -35,10 +41,6 @@ public class JwtProvider {
     public String extractUsername(String token){
         return extractClaims(token).getSubject();
     }
-//    만료 시간 검증
-//    public boolean isTokenExpired(Claims claims){
-//        return claims.getExpiration().before(new Date());
-//    }
 
     public String resolveToken(HttpServletRequest request){
         String authorization = request.getHeader("Authorization");
@@ -47,4 +49,9 @@ public class JwtProvider {
         }
         return null;
     }
+
+//    만료 시간 검증
+//    public boolean isTokenExpired(Claims claims){
+//        return claims.getExpiration().before(new Date());
+//    }
 }
