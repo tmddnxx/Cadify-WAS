949e6039ede314d1edf2f89305999fecdeaac2d0
tmddnxx <lsw6878@naver.com>
2024-11-04 16:08:23 +0900

    feat : S3 Setting

--- cadifyWAS/src/main/java/com/cadify/cadifyWAS/Controller/S3RestController.java
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/Controller/S3RestController.java
@@ -1,2 +1,44 @@
-package com.cadify.cadifyWAS.Controller;public class S3RestController {
+package com.cadify.cadifyWAS.Controller;
+
+import com.amazonaws.services.s3.model.S3Object;
+import com.cadify.cadifyWAS.Service.S3UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/s3")
+public class S3RestController {
+
+
+    private final S3UploadService s3Service;
+
+    @Autowired
+    public S3RestController(S3UploadService s3Service) {
+        this.s3Service = s3Service;
+    }
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            s3Service.uploadFile(file.getOriginalFilename(), file);
+            return ResponseEntity.ok("File uploaded successfully!");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading file");
+        }
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<S3Object> downloadFile(@PathVariable String fileName) {
+        S3Object s3Object = s3Service.downloadFile(fileName);
+        return ResponseEntity.ok(s3Object);
+    }
+
+
 }
--- cadifyWAS/src/main/java/com/cadify/cadifyWAS/Service/S3UploadService.java
+++ cadifyWAS/src/main/java/com/cadify/cadifyWAS/Service/S3UploadService.java
@@ -1,4 +1,38 @@
 package com.cadify.cadifyWAS.Service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
 public class S3UploadService {
+
+    private final AmazonS3 amazonS3;
+    private final String bucketName;
+
+    @Autowired
+    public S3UploadService(AmazonS3 amazonS3, @Value("${aws.s3.bucket.name}") String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
+
+    public void uploadFile(String fileName, MultipartFile file) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        InputStream inputStream = file.getInputStream();
+
+        amazonS3.putObject(bucketName, fileName, inputStream, metadata);
+    }
+
+    public S3Object downloadFile(String fileName) {
+        return amazonS3.getObject(bucketName, fileName);
+    }
+
 }
